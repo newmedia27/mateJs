dbf160ef11e8063782e97b95d508d7804121113d
Sviatoslav <newmedia27@gmail.com>
2018-10-06 18:12:56 +0300

    add  2 homeWork

--- lesson2/loops-statements.js
+++ lesson2/loops-statements.js
@@ -26,14 +26,14 @@ Write a JavaScript program which iterates the integers
 
 for (i = 1; i <= 100; i++) {
     let num = 0;
-    if(i % 3 == 0 && i % 5 == 0)  {
+    if (i % 3 == 0 && i % 5 == 0) {
         num = 'FizzBuzz';
 
     } else if (i % 5 == 0) {
         num = 'Buzz';
     } else if (i % 3 == 0) {
         num = 'Fiz';
-    }else {
+    } else {
         num = i;
     }
     document.write(num);
@@ -44,3 +44,59 @@ for (i = 1; i <= 100; i++) {
 
 Write a JavaScript program to compute
  the greatest common divisor (GCD) of two positive integers*/
+
+function nod(a, b) {
+    let result;
+    if (b > 0) {
+        result = a % b;
+        return nod(b, result);
+    } else {
+        return Math.abs(a);
+    }
+
+}
+
+console.log(nod(96, 150));
+
+/*Напишите программу JavaScript, чтобы суммировать кратные 3 и 5 менее 1000.*/
+
+function sum(n) {
+    let result = 0;
+    for (let i = 0; i < n; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            result += i;
+        }
+    }
+    return result;
+
+}
+
+console.log(sum(1000));
+
+/*Console log all numbers from range 10-90 that divide to 5 and 3.*/
+
+function sum2() {
+    for (let i = 10; i <= 90; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log(i);
+        }
+    }
+}
+
+sum2();
+
+/*Console log only odd numbers from range 11-70 in reverse order starting with 70.*/
+
+for (let i = 70; i >= 11; i--) {
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+
+}
+/*Console log only even numbers that divide to 2 and 3 from range 13-78 in reverse order.*/
+
+for (let i = 78; i >= 13; i--) {
+    if (i % 3 == 0 && i % 2 == 0) {
+        console.log(i);
+    }
+}
